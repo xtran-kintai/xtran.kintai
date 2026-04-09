f27da9edc3f2eac2964f407926ad90b67cd10d72
sakurai-cyber <sakurai@xtran.co.jp>
2026-04-09 13:16:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'kintai-v1';
+const FILES = [
+  './kintai_multi_user.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
